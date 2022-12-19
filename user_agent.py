1a515dc8c82d27bdb612891f129c8333a1820fc4
brianameyer <117409534+brianameyer@users.noreply.github.com>
2022-12-18 22:23:39 -0500

    Add files via upload

---
+++ user_agent.py
@@ -0,0 +1,37 @@
+import requests
+
+url = "https://query.wikidata.org/sparql"
+
+sparql = """
+
+#African American Cemeteries
+SELECT ?item ?itemLabel 
+WHERE 
+{
+  ?item wdt:P31 wd:Q39614. # Must be of a African-American cemetery
+  ?item wdt:P17 wd:Q30.
+ 
+ OPTIONAL {
+    ?article schema:about ?item .
+    ?article schema:inLanguage "en" .
+    FILTER (SUBSTR(str(?article), 1, 25) = "https://en.wikipedia.org/")
+  } 
+  
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
+}
+
+"""
+
+params = {
+	
+	'query' : sparql
+}
+
+headers = {
+  'Accept' : 'application/sparql-results+json;charset=utf-8'
+  'User-Agent' : 'USER Brianameyer'
+}
+
+r = requests.get(url, params=params, headers=headers)
+
+print(r.text)
